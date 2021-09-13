8bc898a21e638c0fa3cf8ac53baa3c2070c83d79
Sourabh Singh <17singh1999@gmail.com>
2021-09-13 18:02:17 +0530

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  // constructor() { }
+  public input: any;
+  public constructor(){
+    // this.input={
+    //   "name":"",
+    //   "email":"",
+    //   "address":"",
+    //   "gender":"" 
+    // }
+  }
+  myForm !:FormGroup;
+
+  ngOnInit(): void {
+    this.myForm=new FormGroup({
+      name:new FormControl(null,Validators.required),
+      zip:new FormControl(null,Validators.required),
+      email: new FormControl(null, [Validators.required,Validators.email]),
+      right: new FormControl(Validators.required)
+      
+    })
+  }
+reset(){
+  this.myForm.reset('')
+}
+
+
+  onSubmit(){
+    // this._tabledataService.enroll(this.input)
+    // Subscribe(
+    //   data=>console.log('Success')
+    // )
+    // error=>console.error('Error!!',error);
+    //localStorage.setItem("profile",JSON.stringify(this.input));
+    //console.log(this.input);
+    console.log(this.myForm.value);
+    // console.log(localStorage.getItem("profile"));
+
+  }
+
+}
+
+// export class AppComponent {
+
+  
+
+//   form = new FormGroup({
+
+//     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+
+//     email: new FormControl('', [Validators.required, Validators.email]),
+
+//     body: new FormControl('', Validators.required)
+
+//   });
+
+  
+
+  // get f(){
+
+  //   return this.form.controls;
+
+  // }
+
+  // onSubmit(){
+  //   // this._tabledataService.enroll(this.input)
+  //   // Subscribe(
+  //   //   data=>console.log('Success')
+  //   // )
+  //   // error=>console.error('Error!!',error);
+  //   //localStorage.setItem("profile",JSON.stringify(this.input));
+  //   //console.log(this.input);
+  //   console.log(this.form.value);
+  //   // console.log(localStorage.getItem("profile"));
+
+  // }
+
+ 
+  
+
+
